885600c27b7385cd2c506a48baff184290eedcb0
Raymond Ha <raymond@shraymonks.com>
2023-03-30 11:10:33 -0700

    Disable tested branch warning

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -352,7 +352,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
   },
 
   EventName.startupMaster: {
-    ET.PERMANENT: startup_master_alert,
+    ET.PERMANENT: StartupAlert("Be ready to take over at any time")
   },
 
   # Car is recognized, but marked as dashcam only
